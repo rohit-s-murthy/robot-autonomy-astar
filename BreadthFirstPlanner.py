9b09c9d61b38d2ecdd459561b74f6e615b4901c7
Basel Alghanem <basel@umich.edu>
2018-03-25 16:56:54 -0400

    works but maybe inefficient

--- BreadthFirstPlanner.py
+++ BreadthFirstPlanner.py
@@ -17,31 +17,31 @@ class BreadthFirstPlanner(object):
         
         parent = dict()
         to_visit = deque()
-        print(start_config)
+        # print(start_config)
         s_id = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
         g_id = self.planning_env.discrete_env.ConfigurationToNodeId(goal_config)
         parent[s_id] = None
         to_visit.append(s_id)
         
-        print "starting planning"
-        print g_id
+        # print "starting planning"
+        # print g_id
         while to_visit:
             s_id = to_visit.popleft()
-            print "visiting", s_id
+            # print "visiting", s_id
             succ = self.planning_env.GetSuccessors(s_id)
-            print "successors", succ
+            # print "successors", succ
             for s in succ:
                 if s not in parent:
                     to_visit.append(s)
                     parent[s] = s_id
-                    print s_id, s
+                    # print s_id, s
             if g_id in parent:
                 break
 
         plan_ids = [g_id]
         plan = [goal_config]
         while parent[plan_ids[-1]] is not None:
-            config_to_add = self.planning_env.NodeIdToConfiguration(parent[plan_ids[-1]])
+            config_to_add = self.planning_env.discrete_env.NodeIdToConfiguration(parent[plan_ids[-1]])
             plan.append(config_to_add)
             plan_ids.append(parent[plan_ids[-1]])
         plan.reverse()   
