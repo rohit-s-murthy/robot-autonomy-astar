a312abd47ab1c23b0e06fd11f9ec400f32733e9c
Harikrishnan <harikrishnan.suresh12@gmail.com>
2018-03-25 18:49:12 -0400

    added print statements for what they asked for

--- DepthFirstPlanner.py
+++ DepthFirstPlanner.py
@@ -1,5 +1,6 @@
 import sys
-sys.setrecursionlimit(10001)
+import time
+sys.setrecursionlimit(50000)
 
 class DepthFirstPlanner(object):
     
@@ -8,9 +9,10 @@ class DepthFirstPlanner(object):
         self.visualize = visualize
         # self.nodes = dict()
         self.parent = dict() #easier to relate
-            
+        self.expanded_nodes = 0 #counting total nodes expanded
 
     def DFS(self,current_nodeId,goal_nodeId):
+        self.expanded_nodes = self.expanded_nodes+1
         succ = self.planning_env.GetSuccessors(current_nodeId)
         for idx in succ:
             #exit from function if we reach goal
@@ -22,15 +24,16 @@ class DepthFirstPlanner(object):
                 self.parent[idx]=current_nodeId
                 self.DFS(idx,goal_nodeId)
 
-
     def Plan(self, start_config, goal_config):
-        
-        plan = []        
+
+        start_time = time.time()
+        plan = []
         # TODO: Here you will implement the depth first planner
         #  The return path should be a numpy array
         #  of dimension k x n where k is the number of waypoints
         #  and n is the dimension of the robots configuration space
-        
+
+        visited_nodes = 0
         #convert to node id to pass to helper function
         start_nodeId = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
         goal_nodeId = self.planning_env.discrete_env.ConfigurationToNodeId(goal_config)
@@ -42,16 +45,22 @@ class DepthFirstPlanner(object):
         idx = goal_nodeId
         #generating plan reverse
         plan.append(goal_config)
-        #to stop backtracking later        
+        #to stop backtracking later
         self.parent[start_nodeId] = None
 
         while self.parent[idx] is not None:
+            visited_nodes = visited_nodes+1
             #convert to config space to append to plan
             current_config = self.planning_env.discrete_env.NodeIdToConfiguration(self.parent[idx])
             plan.append(current_config)
             idx = self.parent[idx]
 
         plan.append(start_config)
+        visited_nodes = visited_nodes + 1
         plan.reverse()
+        stop_time = time.time()
         print(plan)
+        print("path length = %s" %(visited_nodes*self.planning_env.resolution))
+        print("time taken = %s" %(stop_time-start_time))
+        print("total nodes expanded = %s" %(self.expanded_nodes))
         return plan
